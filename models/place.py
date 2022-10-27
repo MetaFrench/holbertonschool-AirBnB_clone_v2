b34626cc97f0a3f5453d6532ac9c70ecc2790c32
Clayton Christian <103296303+claybowl@users.noreply.github.com>
2022-10-27 06:48:58 -0500

    updated place with review setter and relationsips

--- models/place.py
+++ models/place.py
@@ -26,3 +26,28 @@ class Place(BaseModel):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship('Review', backref='place',
+                               cascade='all, delete-orphan')
+        amenities = relationship('Amenity',
+                                 secondary='place_amenity',
+                                 backref='places', viewonly=False)
+    else:
+        @property
+        def reviews(self):
+            """Getter attribute in case of file storage"""
+            return [review for review in models.storage.all(Review)
+                    if review.place_id == self.id]
+
+        @property
+        def amenities(self):
+            """Getter attribute in case of file storage"""
+            return [amenity for amenity in models.storage.all(Amenity)
+                    if amenity.id in self.amenity_ids]
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Setter method for amenities"""
+            if (type(obj) == Amenity):
+                self.amenity_ids.append(obj.id)
