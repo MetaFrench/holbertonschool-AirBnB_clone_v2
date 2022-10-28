6eca0d77afd79dedc9ff3e360df8e60211fad839
clybowl <4941@holbertonschool.com>
2022-10-27 17:59:49 -0700

    reverted for checks

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-<<<<<<< HEAD
-import os
-
-if os.getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-    storage.reload()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-    storage.reload()
-=======
 from os import getenv
 
 
@@ -24,4 +12,3 @@ else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
->>>>>>> efbf42c22502b8589acf0b08781b921a80392a0e
--- models/state.py
+++ models/state.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
+from os import getenv
+from models.city import City
 from sqlalchemy.orm import relationship
-import os
+import models
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship(
-            "City",
-            backref="state",
-            cascade="all, delete-orphan"
-        )
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        # sets relationship between state and city
+        cities = relationship('City', backref='state',
+                                cascade='all, delete, delete-orphan')
     else:
         @property
         def cities(self):
-            """Returns a list of City instances with state_id = id"""
-            cities = []
-            for thing in models.storage.all(City).values():
-                if thing.state_id == self.id:
-                    cities.append(thing)
-            return cities
+            """returns list of City instances associated with state.id"""
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
