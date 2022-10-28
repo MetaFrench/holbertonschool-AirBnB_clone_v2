5f8d9bcbda24fb214ba6897624d29672b34de9aa
clybowl <4941@holbertonschool.com>
2022-10-27 18:05:59 -0700

    reverting for all checks

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,11 @@ import json
 
 
 class FileStorage:
+"""This module defines a class to manage file storage for hbnb clone"""
+import json
+
+
+class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
@@ -45,10 +50,10 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
@@ -68,3 +73,4 @@ class FileStorage:
                 self.__objects.pop(key)
         else:
             return
+
