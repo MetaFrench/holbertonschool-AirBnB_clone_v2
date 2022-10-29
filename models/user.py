36c0f40afb06b4c04d2c0349ac7b718ba74719ef
clybowl <4941@holbertonschool.com>
2022-10-28 20:01:01 -0700

    test

--- models/user.py
+++ models/user.py
@@ -8,13 +8,6 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
-    def __init__(self, username: str, password_hash: str, email: str):
-        """Initiates self"""
-        [...]
-
-        # Initialize base class
-        super(User, self).__init__()
-
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
