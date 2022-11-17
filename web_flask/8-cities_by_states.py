74047a5dee1fd1f197e6fa92b027eb6910c012a5
root <nick.french0307@gmail.com>
2022-11-16 21:02:35 -0800

    eight

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+from flask import Flask, render_template
+import models
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown(content):
+    """ removes current SQLAlchemy sesh """
+    models.storage.close()
+
+
+@app.route('/cities_by_states')
+def states_list():
+    """ prints list of states """
+    states = models.storage.all(models.state.State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
